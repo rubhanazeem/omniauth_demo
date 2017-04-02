986d1c2eab956e79dc7afecad61aa2ada65bbebe
rubhanazeem <rubhanazeem@gmail.com>
2017-04-02 18:15:44 +0500

    facebook callback added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,8 @@ class SessionsController < Devise::OmniauthCallbacksController
 	def twitter
 		
 	end
+
+	def facebook
+		
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:twitter]
+         :omniauthable, :omniauth_providers => [:twitter, :facebook]
 end
